9de0426c14d1f00e88586ce72faa56e4abd342ca
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2020-01-20 22:06:19 +0200

    finish teacher

--- pro.asm
+++ pro.asm
@@ -8,6 +8,11 @@
 	Teacher DB 'teacher.bin', 0
 	Policer DB 'police.bin',0
 	Filehandle DW ?
+	Jops DB 'Teacher','Policer','Doctor','Soldier','Lawyer','Player','Actor'
+	Guess_who     db 'Guess Who .....?'
+	Teacher_Jop   db 'I help people to learn,work in a classroom.I teach young children in kindergarten'
+    Teacher_Jop_c db 'and primary schools. I teach older children in middle, junior high and high schools.'
+                    
 
 	.CODE
 	 ; __  __     _     ___   _  _ 
@@ -25,27 +30,56 @@
 		;  mov ah,0
 		;  mov al,03
 		;  int 10h 
+   
+  
 
-		mov ax,4f02h
-		MOV BX,105H ; 1024*786 256 COLORS
-		INT 10H 
-		
+;   _______                         _                   
+;  |__   __|                       | |                  
+;     | |      ___    __ _    ___  | |__     ___   _ __ 
+;     | |     / _ \  / _` |  / __| | '_ \   / _ \ | '__|
+;     | |    |  __/ | (_| | | (__  | | | | |  __/ | |   
+;     |_|     \___|  \__,_|  \___| |_| |_|  \___| |_|   
+	    call Video_mode
 		OpenFile Teacher,Filehandle
 		ReadData Filehandle,Data_F,WIDTH,HEIGHT
 		LEA BX , Data_F
 		Draw BX,WIDTH,HEIGHT,0,0
+		CloseFile Filehandle
+		Print Teacher_Jop,81,27,3,04
+		Print Teacher_Jop_c,84,27,5,04
+		Print Guess_who,16,65,8,14
+	   
 
-    MOV AH , 0
-    INT 16h
+         MOV AH , 0
+         INT 16h
 
+
+;    ____    ______   ______   _                      
+;   / __ \  |  ____| |  ____| (_)                     
+;  | |  | | | |__    | |__     _    ___    ___   _ __ 
+;  | |  | | |  __|   |  __|   | |  / __|  / _ \ | '__|
+;  | |__| | | |      | |      | | | (__  |  __/ | |   
+;   \____/  |_|      |_|      |_|  \___|  \___| |_|  
+		call Video_mode
 	    OpenFile Policer,Filehandle
 		ReadData Filehandle,Data_F,WIDTH,HEIGHT
 		LEA BX , Data_F
+		CloseFile Filehandle
 		Draw BX,WIDTH,HEIGHT,0,0
-
+		 MOV AH , 0
+    INT 16h
     
-CloseFile Filehandle
 
+
+Video_mode PROC
+	mov ax,4f02h
+		MOV BX,105H ; 1024*786 256 COLORS
+		INT 10H 
+	ret	
+Video_mode endp
 
   END MAIN 
+
+
+
 		
